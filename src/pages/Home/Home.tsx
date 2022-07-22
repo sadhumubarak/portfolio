87568c6c31c7eb1bcc6ca6df442ec091f6551c0b
sadhamhussain <sadhamhussain@valorpaytech.com>
2022-07-22 18:38:13 +0530

    scrolls

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import { Grid } from "@mui/material";
 import React, { Fragment } from "react";
 import ViewPort from "../ViewPort";
@@ -7,9 +8,29 @@ import myData from "../../assets/data/myData.json";
 import Knowledge from "../Knowledge";
 import NavPanel from "../../shared/ActionButtons";
 import Header from "../../shared/Header/Header";
+// import useInteraction from "../../shared/useIntersection";
 type Props = {};
 
-export default function Home({}: Props) {
+const Home = ({}: Props) => {
+  useEffect(() => {
+    return () => {
+      window.addEventListener("scroll", function () {
+        var element = document.querySelector("#experience");
+        var position = element.getBoundingClientRect();
+
+        // checking whether fully visible
+        if (position.top >= 0 && position.bottom <= window.innerHeight) {
+          
+        }
+
+        // checking for partial visibility
+        if (position.top < window.innerHeight && position.bottom >= 0) {
+          console.log("Element is partially visible in screen");
+        }
+      });
+    };
+  }, []);
+
   return (
     <Fragment>
       <Header />
@@ -20,4 +41,6 @@ export default function Home({}: Props) {
       <Knowledge knowledge={myData.knowledge} />
     </Fragment>
   );
-}
+};
+
+export default Home;
