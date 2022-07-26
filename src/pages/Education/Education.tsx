c3141e84b8459d31773d5798da6bd62c190bfccc
sadhamhussain <sadhamhussain@valorpaytech.com>
2022-07-26 16:01:23 +0530

    updates

--- src/pages/Education/Education.tsx
+++ src/pages/Education/Education.tsx
@@ -3,6 +3,8 @@ import ContentBox from "../../shared/ContentBox";
 import { FaUserGraduate } from "react-icons/fa";
 import SectionTitle from "../../shared/SectionTitle";
 import { GiSchoolBag, GiTiedScroll } from "react-icons/gi";
+import expPoster from "../../assets/img/clip.png";
+
 type EduTypes = {
   education: {
     course: string;
@@ -17,69 +19,29 @@ export default function Education({ education }: EduTypes) {
   return (
     <ContentBox contentId="education">
       <Grid container sx={{ display: "flex", alignItems: "center" }}>
+{/* 
+        <Box
+        component="img"
+        src={ expPoster }
+        alt="expPoster"
+        sx={{
+          top: 0,
+          right: 0,
+          position: "absolute",
+          width: "100px",
+          height: "100%",
+          zIndex: "-1",
+          border: "none",
+        }}
+      /> */}
         <SectionTitle countNo="03" title="Education" />
-        <Grid item xs={12}>
+        <Grid item xs={12} >
+{/*           
           <Grid container>
             {education.map((edu, i) => (
-              <Grid
-                item
-                xs={12}
-                md={12}
-                lg={4}
-                key={i}
-                sx={{ margin: { lg: "0 auto", md: "0 auto 40px" } }}
-              >
-                <Box
-                  sx={{
-                    width: "90%",
-                    height: "100%",
-                    textAlign: "center",
-                    position: "relative",
-                    // boxShadow: "0 0 3px #dfdfdf",
-                    backgroundColor: "#353655",
-                    margin: "0 auto",
-                    borderRadius: "5px",
-                    padding: "20px 30px",
-                  }}
-                >
-                  <Box
-                    sx={{
-                      position: "absolute",
-                      width: "60px",
-                      height: "60px",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      borderRadius: "50px",
-                      backgroundColor: "#575878",
-                      left: "-5px",
-                      top: "-25px",
-                      transform: "translateX(-50%)",
-
-                      "& svg": {
-                        fontSize: "1.2rem",
-                        color: "#ffe118",
-                      },
-                    }}
-                  >
-                    {i === 0 ? (
-                      <GiTiedScroll />
-                    ) : i === 1 ? (
-                      <FaUserGraduate />
-                    ) : (
-                      <GiSchoolBag />
-                    )}
-                  </Box>
-                  <Grid item sx={{ paddingTop: "10px" }}>
-                    <Typography variant="h6">{edu.course}</Typography>
-                    <Typography variant="subtitle1">{edu.year}</Typography>
-                    <Typography variant="subtitle1">{edu.institute}</Typography>
-                    <Typography variant="subtitle1">{edu.location}</Typography>
-                  </Grid>
-                </Box>
-              </Grid>
+             
             ))}
-          </Grid>
+          </Grid> */}
         </Grid>
       </Grid>
     </ContentBox>
