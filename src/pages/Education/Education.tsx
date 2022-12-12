bc4a5ddc8d5ca1c49c7bb155c230f9ca1e6c1066
sadhumubarak <smshussain007@gmail.com>
2022-12-12 17:30:34 +0530

    updated education

--- src/pages/Education/Education.tsx
+++ src/pages/Education/Education.tsx
@@ -19,8 +19,8 @@ export default function Education({ education }: EduTypes) {
   return (
     <ContentBox contentId="education">
       <Grid container sx={{ display: "flex", alignItems: "center" }}>
-{/* 
-        <Box
+
+        {/* <Box
         component="img"
         src={ expPoster }
         alt="expPoster"
@@ -35,13 +35,21 @@ export default function Education({ education }: EduTypes) {
         }}
       /> */}
         <SectionTitle countNo="03" title="Education" />
-        <Grid item xs={12} >
-{/*           
-          <Grid container>
-            {education.map((edu, i) => (
-             
-            ))}
-          </Grid> */}
+        <Grid container xs={12} rowSpacing={2}>
+          {
+            education.map((edu, i) => (
+              <Grid item sx={{display:'flex',width:'100%', position:'relative', mb:"35px"}}>
+                <Grid item xs={3} sx={{background:'#575878', position:'relative', left:'20px', zIndex:'2',p:'15px', height:'fit-content', width:'fit-content'}}>
+                  <Typography>JUN 2017 - 2022</Typography>
+                </Grid>
+                <Grid item xs={9} sx={{background:'#333',position:'relative', top:'40px', padding:'15px 30px'}}>
+                  <Typography variant="h5">Course Name</Typography>
+                  <Typography variant="subtitle2">Insitute Name</Typography>
+                </Grid>
+              </Grid>
+            ))
+          }
+
         </Grid>
       </Grid>
     </ContentBox>
